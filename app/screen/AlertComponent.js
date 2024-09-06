30d10d842b1e3688ccca8689b75eab2c852215a2
para-commando <anirudh.nayak.172k@gmail.com>
2024-09-07 00:25:04 +0530

    box shadows and some styles

--- app/screen/AlertComponent.js
+++ app/screen/AlertComponent.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { Alert, Image, Platform, Pressable, StatusBar } from 'react-native';
+import {
+  Alert,
+  Button,
+  Image,
+  Platform,
+  Pressable,
+  StatusBar,
+  Text,
+} from 'react-native';
 import { SafeAreaView } from 'react-native';
 
-function AlertComponent({alertMessage}) {
+function AlertComponent({ alertMessage }) {
   const handleImgPress = (params) => {
     Alert.alert('Alert title text', alertMessage, [
       {
@@ -19,11 +27,17 @@ function AlertComponent({alertMessage}) {
       },
     ]);
   };
+
   return (
     <SafeAreaView
       style={{
         flex: 1,
         paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+        borderRadius: 25,
+        paddingHorizontal: 12,
+        backgroundColor: 'black',
+        marginHorizontal: 12,
+        marginVertical: 2,
       }}
     >
       <Pressable onPress={handleImgPress}>
@@ -37,7 +51,34 @@ function AlertComponent({alertMessage}) {
           source={require('../assets/para-commando.jpg')}
           fadeDuration={1000}
         />
+        <Button title='ooooow2w2' color='green' />
       </Pressable>
+      <Pressable
+          style={{
+            backgroundColor: 'white', // Set button background color
+            padding: 10,
+            borderRadius: 5,
+            shadowColor: 'blue', // Set shadow color
+            shadowOffset: {
+              width: 80,
+              height: 50,
+            },
+            shadowOpacity: 0.4,
+            shadowRadius: 4,
+            elevation: 10,  
+          }}
+          onPress={handleImgPress} // Use this or any other handler
+        >
+          <Text
+            style={{
+              color: 'green', // Set text color
+              textAlign: 'center',
+            }}
+          >
+            ooooorrrrrrrr
+          </Text>
+        </Pressable>
+    
     </SafeAreaView>
   );
 }
